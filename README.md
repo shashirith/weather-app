79dc6d0c33c8ebaa95de9647de46a980e9fdbbbe
Shashirith Sonu <shashirithsonu@Shashiriths-MacBook-Pro.local>
2023-07-12 11:43:42 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Weather-app
+simple weather app created using reactJs. i have fetched the data from OpenWeatherMap API and GeoDB API.
+
---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "737fdbdab8msh84b5ae662fe6dd6p16a0b3jsnffd60fcc5887",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "490d3fc0ca12a0031296d76a274e2be6"; // enter your key from openweather API
